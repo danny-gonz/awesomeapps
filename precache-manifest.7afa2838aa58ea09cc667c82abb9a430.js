928f7e48b6a3a685d862b73142ed9c83d1cd1af2
danny-gonz <dpg2012@hotmail.com>
2020-08-07 15:48:11 -0700

    Updates

---
+++ precache-manifest.7afa2838aa58ea09cc667c82abb9a430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397296ec73e35037fcb5c4e448eb7ad5",
+    "url": "/awesomeapps/index.html"
+  },
+  {
+    "revision": "0dfc93c710e41122a4a5",
+    "url": "/awesomeapps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a46fa89de4c35be3e517",
+    "url": "/awesomeapps/static/js/2.ea177ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awesomeapps/static/js/2.ea177ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfc93c710e41122a4a5",
+    "url": "/awesomeapps/static/js/main.3834a14c.chunk.js"
+  },
+  {
+    "revision": "57bfebb6890ac6455c93",
+    "url": "/awesomeapps/static/js/runtime-main.adace147.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awesomeapps/static/media/logo.5d5d9eef.svg"
+  }
+]);
